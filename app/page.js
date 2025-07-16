f08b23b60a29fd19e367e435820725f694b9250a
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-07-16 15:32:53 +0300

    update page

--- app/page.js
+++ app/page.js
@@ -3,6 +3,9 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <header className="row-start-1 flex gap-[24px] flex-wrap items-center justify-center">
+        header
+      </header>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="text-9xl font-sligoil">peep</h1>
       </main>
