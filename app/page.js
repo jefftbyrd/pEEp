f4b96c9743e37b0feb7a09ea68e42632046b35eb
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-07-25 17:20:02 +0300

    update home page

--- app/page.js
+++ app/page.js
@@ -8,12 +8,15 @@ export default function Home() {
       <header className="row-start-1 flex gap-[24px] flex-wrap items-center justify-center">
         <h1 className="text-9xl">peep</h1>
       </header>
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start w-2/3">
         <Module1 />
         <Module2 />
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        footer
+        <p>
+          {' '}
+          audio visualization library by <a>jeff t byrd</a>
+        </p>
       </footer>
     </div>
   );
